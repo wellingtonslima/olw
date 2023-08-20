299e3a2e881e4a5af475fe281350c880028a504e
Wellington Lima <wellington.lima@capesesp.com.br>
2023-08-20 11:18:44 -0300

    Added OpenAi

--- app/Models/SalesCommission.php
+++ app/Models/SalesCommission.php
@@ -9,4 +9,19 @@ class SalesCommission extends Model
     protected $table = "sales_commission_view";
     public $incrementing = false;
     public $timestamp = false;
+
+    public static function getColumns(){
+        return [
+            'company',
+            'seller',
+            'client',
+            'city',
+            'state',
+            'sold',
+            'sold_at',
+            'status',
+            'total_amount',
+            'commission'
+        ];
+    }
 }
